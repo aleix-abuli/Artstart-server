14cf3e736497744c58346792f3b70e32d523f0ab
aleix-abuli <aleixabuli088@gmail.com>
2022-05-31 17:47:00 +0200

    config done

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port http://localhost:${PORT}`);
+    console.log(`Artstart Server listening on port http://localhost:${PORT}! Yay!!!`);
 });
